c6a250454238878bf8f5e221128c1bf73f4344dc
Mohit2005123 <mohitmongia2005@gmail.com>
2024-08-31 20:57:07 +0530

    Added spreadsheetid

--- models/spreadmodel.js
+++ models/spreadmodel.js
@@ -1,24 +1,28 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 // Define User Schema
 const spreadschema = mongoose.Schema(
-{ data: {
-    type: [[mongoose.Schema.Types.Mixed]], // 2D array of mixed types
-    default: [] // Default to an empty array
-  },
-  name:{
-    type: String,
+  {
+    data: {
+      type: [[mongoose.Schema.Types.Mixed]], // 2D array of mixed types
+      default: [], // Default to an empty array
+    },
+    name: {
+      type: String,
       required: true,
+    },
+    spreadsheetId: {
+      type: String, // Assuming the spreadsheetId is a string
+      required: true, // Set this to false if it's not required
+      unique: true, // Optional: Ensures that each spreadsheetId is unique
+    },
   },
-},
-
   {
     timestamps: true,
   }
 );
 
-
 const spreadsheetmodel = mongoose.model('Spreadsheet', spreadschema);
 
 module.exports = spreadsheetmodel;
+
