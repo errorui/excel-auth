02f904fb9b7cdf4d885e01672a146a6f8c39034a
raj <rajraman21211@gmail.com>
2024-08-31 23:54:47 +0530

    efficient algo implemeneted

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -9,15 +9,16 @@ const {
 } = require('../controllers/spreasheetcontroller');
 
 
-// Route to create a spreadsheet and update users' projects
+// Route to create a spreadsheet and update users' projects--checked
 router.post('/create-spreadsheet', createSpreadsheetAndUpdateUsers);
+//checked
 // Route to check if a given spreadsheetId is present in the user's projects array by email
-router.get('check/:email/:spreadsheetId', checkSpreadsheetId);
+// router.get('check/:email/:spreadsheetId', checkSpreadsheetId);
 
-// Route to get spreadsheet content by spreadsheetId
+// Route to get spreadsheet content by spreadsheetId-checked
 router.get('/spreadsheet/:spreadsheetId', getSpreadsheetContent);
 
-router.post('/spreadsheet/:spreadsheetId', getSpreadsheetContentByChecking);
+router.post('/check/:spreadsheetId', getSpreadsheetContentByChecking);
 
 // Route to update spreadsheet data and name by spreadsheetId
 router.post('/spreadsheet/:spreadsheetId', updateSpreadsheet);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -34,3 +34,61 @@ router.route("/logout").get(protect, (req, res) => {
 });
 
 module.exports = router;
+/*
+[
+    {
+        "_id": "66cf7c265e532b2919c279ef",
+        "username": "testuser",
+        "email": "testuser@example.com",
+        "createdAt": "2024-08-28T19:36:06.990Z",
+        "updatedAt": "2024-08-28T19:36:06.990Z",
+        "__v": 0,
+        "projects": []
+    },
+    {
+        "_id": "66d09c45923e4bf3e81576a5",
+        "username": "raj",
+        "email": "rajraman@gmail.com",
+        "createdAt": "2024-08-29T16:05:25.044Z",
+        "updatedAt": "2024-08-29T16:05:25.044Z",
+        "__v": 0,
+        "projects": []
+    },
+    {
+        "_id": "66d143c16b5577585850a1a2",
+        "username": "dev",
+        "email": "dev@gmail.com",
+        "createdAt": "2024-08-30T04:00:01.863Z",
+        "updatedAt": "2024-08-30T04:00:01.863Z",
+        "__v": 0,
+        "projects": []
+    },
+    {
+        "_id": "66d147685778a7a87061187c",
+        "username": "dev1",
+        "email": "dev1@gmail.com",
+        "createdAt": "2024-08-30T04:15:36.571Z",
+        "updatedAt": "2024-08-30T04:15:36.571Z",
+        "__v": 0,
+        "projects": []
+    },
+    {
+        "_id": "66d20db0fd9fb228aa403a34",
+        "username": "dev2",
+        "email": "dev2@gmail.com",
+        "createdAt": "2024-08-30T18:21:36.421Z",
+        "updatedAt": "2024-08-30T18:21:36.421Z",
+        "__v": 0,
+        "projects": []
+    },
+    {
+        "_id": "66d357cdd14c88d35cd7d6d8",
+        "username": "Mohit",
+        "email": "Mohit@gmail.com",
+        "projects": [],
+        "createdAt": "2024-08-31T17:50:05.705Z",
+        "updatedAt": "2024-08-31T17:50:05.705Z",
+        "__v": 0
+    }
+]
+*/ 
