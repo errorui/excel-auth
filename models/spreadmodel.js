ecaac6a09e317790346b437409a2dc7718081add
raj <rajraman21211@gmail.com>
2024-08-31 20:46:46 +0530

    merge

--- models/spreadmodel.js
+++ models/spreadmodel.js
@@ -3,11 +3,16 @@ const bcrypt = require('bcrypt');
 
 // Define User Schema
 const spreadschema = mongoose.Schema(
-//   {}
 { data: {
     type: [[mongoose.Schema.Types.Mixed]], // 2D array of mixed types
     default: [] // Default to an empty array
-  }},
+  },
+  name:{
+    type: String,
+      required: true,
+  },
+},
+
   {
     timestamps: true,
   }
