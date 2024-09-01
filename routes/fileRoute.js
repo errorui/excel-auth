418727ce585fdee09fd6eaddbc4879c2c9107f9c
Mohit2005123 <mohitmongia2005@gmail.com>
2024-09-01 14:26:13 +0530

    Removed protect middleware

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -8,22 +8,20 @@ const {
   getSpreadsheetContentByChecking,
   deleteSpreadsheet
 } = require('../controllers/spreasheetcontroller');
-
-const { protect } = require("../middlewares/authmiddleware");
 // Route to create a spreadsheet and update users' projects--checked
-router.post('/create-spreadsheet', protect, createSpreadsheetAndUpdateUsers);
+router.post('/create-spreadsheet', createSpreadsheetAndUpdateUsers);
 //checked
 // Route to check if a given spreadsheetId is present in the user's projects array by email
 // router.get('check/:email/:spreadsheetId', checkSpreadsheetId);
 
 // Route to get spreadsheet content by spreadsheetId-checked
-router.get('/spreadsheet/:spreadsheetId', protect, getSpreadsheetContent);
+router.get('/spreadsheet/:spreadsheetId', getSpreadsheetContent);
 
-router.post('/check/:spreadsheetId',protect,  getSpreadsheetContentByChecking);
+router.post('/check/:spreadsheetId',  getSpreadsheetContentByChecking);
 
 // Route to update spreadsheet data and name by spreadsheetId
-router.post('/spreadsheet/:spreadsheetId', protect, updateSpreadsheet); // Need data and name in the body
+router.post('/spreadsheet/:spreadsheetId', updateSpreadsheet); // Need data and name in the body
 
-router.post('/delete/:spreadsheetId', protect, deleteSpreadsheet);
+router.post('/delete/:spreadsheetId', deleteSpreadsheet);
 
 module.exports = router;
